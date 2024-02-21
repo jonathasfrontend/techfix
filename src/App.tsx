5801ec292e120abf7995b028afc0beb0989bc10f
Jonathas <jonathass5678@gmail.com>
2024-02-21 18:21:20 -0300

    techfixweb

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import bg from '../public/bitmap.png'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Plus } from '@phosphor-icons/react';
+// import { PedidoBanners } from './components/pedidoBanners';
+
+interface Pedido{
+  id: string,
+  name: string,
+  address: string,
+  number: string,
+  cpf: string,
+  email: string,
+  devicedescription: string,
+  defectdescription: string,
+  category: string,
+  status: string,
+  createdAt: string,
+}
+
+function App() {
+
+  const [pedidos, setPedido] = useState<Pedido[]>([]);
+
+  useEffect(()=>{
+    axios('https://servertechfixfr.vercel.app/content/v1/orders').then(response => {
+      // setPedido(response.data);
+      console.log(response.data.order)
+    })
+  }, [])
+
+   return (
+    <div style={{backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center'}} className="w-full h-screen">
+      <header className="w-full flex items-center justify-between p-5">
+        <div>
+          <h1 className="text-2xl text-white font-extrabold">TECH FIX</h1>
+        </div>
+
+        <button className="p-3 bg-green-500 rounded-xl">
+          <Plus size={22} className="text-white" />
+        </button>
+      </header>
+
+      {/* <div className="px-5 w-full">
+      {pedidos.map(pedido => {
+          return (
+            <PedidoBanners
+              key={pedido.id}
+              name={pedido.name}
+              address={pedido.address}
+              number={pedido.number}
+              cpf={pedido.cpf}
+              email={pedido.email}
+              devicedescription={pedido.devicedescription}
+              defectdescription={pedido.defectdescription}
+              status={pedido.status}
+              createdAt={pedido.createdAt}
+            />
+          )
+        })}
+      </div> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/PedidoBanners.tsx
@@ -0,0 +1,51 @@
+import { At, CalendarBlank, CheckCircle, FileText, MapPin, Phone, Plus, SquaresFour, UserCircle, WarningOctagon, Wrench } from "@phosphor-icons/react"
+interface PedidoBannerProps{
+    id: string,
+    name: string,
+    address: string,
+    number: string,
+    cpf: string,
+    email: string,
+    devicedescription: string,
+    defectdescription: string,
+    category: string,
+    status: string,
+    createdAt: string,
+  }
+
+export function PedidoBanners(props: PedidoBannerProps){
+    return(
+        <div className="w-full h-full rounded-md border-solid border-2 text-white border-green-500 bg-[#121214] px-5 py-2 overflow-hidden">
+        <h1 className='flex items-center text-2xl font-semibold'><UserCircle size={25} className='mr-1' />{props.name}</h1>
+        <div className='w-full flex items-center justify-between mt-2'>
+          <h1 className='flex items-center'><Phone size={20} className='mr-1' />{props.number}</h1>
+          <h1 className='flex items-center'><FileText size={20} className='mr-1' />{props.cpf}</h1>
+          <h1 className='flex items-center'><At size={20} className='mr-1' />{props.email}</h1>
+        </div>
+        <span className='flex items-center my-2'><MapPin size={20} className='mr-1'/>{props.address}</span>
+        <div className='flex flex-col gap-2'>
+            <div>
+              <span className='text-base text-zinc-600 font-medium mr-1'>Modelo</span>
+              <div className="bg-[#19191d] p-2 rounded-md">
+                {props.devicedescription}
+              </div>
+            </div>
+            <div>
+              <span className='text-base text-zinc-600 font-medium mr-1'>Descrição do defeito</span>
+              <div className="bg-[#19191d] p-2 rounded-md">
+                {props.defectdescription}
+              </div>
+            </div>
+        </div>
+        <div className='w-full mt-2 flex items-center justify-between'>
+          <div className='flex items-center gap-5'>
+            <h1 className='flex items-center'><SquaresFour size={20} className='mr-1'/>{props.category}</h1>
+            <h1 className='flex items-center'><CalendarBlank size={20} className='mr-1'/>{props.createdAt}</h1>
+          </div>
+          <h1 className='flex items-center bg-green-500 p-1 rounded-md'><CheckCircle size={20} className='mr-1'/>{props.status}</h1>
+          {/* <h1 className='flex items-center bg-sky-600 p-1 rounded-md'><Wrench size={20} className='mr-1'/>Mantutenção</h1>
+          <h1 className='flex items-center bg-red-600 p-1 rounded-md'><WarningOctagon size={20} className='mr-1'/>Fechado</h1> */}
+        </div>
+      </div>
+    )
+}
